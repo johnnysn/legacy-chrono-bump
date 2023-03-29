1e0040bd321d05d797e6b3feae61165b81906c34
Johnny <johnny.gomes@ufjf.br>
2023-03-29 12:54:24 -0300

    feat: Start implementing Player component interval management

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,10 +1,39 @@
-import styles from './Player.module.css';
+import { useContext, useEffect, useState } from "react";
+import BeatsContext from "../../store/beats-context";
+import styles from "./Player.module.css";
 
 const Player = () => {
-  return <div className={styles.player}>
-    <button type='button'>Play</button>
-    <button type='button'>Pause</button>
-  </div>;
+  const ctx = useContext(BeatsContext);
+
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const playClickHandler = event => {
+    setIsPlaying( prevState => !prevState );
+  };
+
+  useEffect(() => {
+    let interval = null;
+    let beat = 1;
+    if (isPlaying) {
+      interval = setInterval(() => {
+        console.log(beat);
+        beat = (beat >= ctx.items.length) ? 1 : beat + 1;
+      }, (60000 / ctx.tempo));
+    }
+  
+    return () => {
+      console.log('CLEANUP');
+      if (interval) clearInterval(interval);
+    };
+  }, [isPlaying, ctx.tempo, ctx.items]);
+
+  return (
+    <div className={styles.player}>
+      <button type="button" onClick={playClickHandler}>
+        <img src="/play-pause-button-96.png" width="90px" height="90px" />
+      </button>
+    </div>
+  );
 };
 
 export default Player;
