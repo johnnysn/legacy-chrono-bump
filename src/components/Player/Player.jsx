758f60930ee730971782e8fa6a48b39a4026e989
Johnny <johnny.gomes@ufjf.br>
2023-03-30 12:49:44 -0300

    perf: Improves timing accuracy by using a third party timer

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { click1 } from "../../store/beats-audio";
 import BeatsContext from "../../store/beats-context";
 import styles from "./Player.module.css";
+import Timer from "../../lib/timer";
 
 const Player = () => {
   const ctx = useContext(BeatsContext);
@@ -13,27 +14,29 @@ const Player = () => {
   };
 
   useEffect(() => {
-    let interval = null;
+    let timer = null;
     let beat = 1;
+
+    const play = () => {
+      const audio = click1;
+      if (audio.paused) {
+        audio.play();
+      } else {
+        audio.currentTime = 0;
+      }
+      ctx.setBeat(beat);
+      beat = beat >= ctx.items.length ? 1 : beat + 1;
+    };
+
     if (isPlaying) {
-      interval = setInterval(() => {
-        //console.log(beat);
-        const audio = click1;
-        if (audio.paused) {
-          audio.play();
-        } else {
-          audio.currentTime = 0;
-        }
-        ctx.setBeat(beat);
-        beat = beat >= ctx.items.length ? 1 : beat + 1;
-      }, 60000 / ctx.tempo);
+      timer = new Timer(play, 60000 / ctx.tempo, {immediate: true});
+      timer.start();
     } else {
       ctx.setBeat(0);
     }
 
     return () => {
-      console.log("CLEANUP");
-      if (interval) clearInterval(interval);
+      if (timer) timer.stop();
     };
   }, [isPlaying, ctx.tempo, ctx.items]);
 
